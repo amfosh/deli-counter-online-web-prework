e184a5512552ea560b00eeec2866158548b05547
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-02 17:01:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,8 +18,7 @@ end
 
 def now_serving(katz_deli)
   puts "There is nobody waiting to be served!"
-else 
-  puts "Currently serving #{name}."
+else puts "Currently serving #{name}."
   array.shift
   array
 end

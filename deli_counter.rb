f880bb279a202c418ff43dd70a7422118df6dad3
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-02 17:06:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 def take_a_number(array, name)
   array.push(name)
-  puts "Welcome #{name}. You are number #{line.length} in line." 
+  puts "Welcome #{name}. You are number #{array.length} in line." 
   puts line
   katz_deli
 end

6718c0b81e3168829782098027fd02f8e17cee79
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-02 17:25:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(people)
   else 
     current_people = "The line is currently:"
     people.each_with_index do |name, index|
-      people << "#{index +1}. "
+      people << "#{index +1}. #{name}."
     end
     puts string
   end

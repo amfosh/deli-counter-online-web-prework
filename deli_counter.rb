898236bdcf3f3e43cc7b8ef9ce721bfc2d3f7cfe
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-02 17:32:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,15 @@
 katz_deli = []
 
-def line(people)
-  if people.size == 0 
+def line(deli)
+  if deli.empty?
     puts "The line is currently empty."
-  else 
-    current_people = "The line is currently:"
-    people.each_with_index do |name, index|
-      current_people << "#{index +1}. #{name}"
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
     end
-  puts current_people
+    puts current_line
+  end
 end
 
 def take_a_number(array, name)
